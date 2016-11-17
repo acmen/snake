2a45219b8c865d19dada1b8e89a55569f998756c
jay.li <acmen19@126.com>
2016-11-17 11:23:19 +0800

    init

---
+++ js/snake.js
@@ -0,0 +1,37 @@
+/**
+ * Created by acmen on 16-11-17.
+ */
+var snake = {
+  options: {
+    canvas: false,
+    food_list: [
+      {
+        score: 1,
+        img: "",
+        probability: 10
+      }
+    ],
+    run_time: -1,
+    snake_data: [],
+    food_data: [],
+    score: 0
+  },
+  init: function(){
+
+  },
+  draw: function(){
+
+  },
+  eat_food: function(){
+
+  },
+  build_snake: function(){
+
+  },
+  build_food: function(){
+
+  },
+  score_change: function(){
+
+  }
+};
