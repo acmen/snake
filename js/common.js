6866a22f5dcab1e6186d5ddb194061d5f2226bde
jay.li <acmen19@126.com>
2016-11-24 21:04:24 +0800

    info

--- js/common.js
+++ js/common.js
@@ -4,8 +4,8 @@
 function init(ele) {
   var width = document.body.clientWidth;
   var height = document.body.clientHeight;
-  //ele.style.width = width+"px";
-  //ele.style.height = height+"px";
+  // ele.style.width = width+"px";
+  // ele.style.height = height+"px";
    ele.style.width = height+"px";
    ele.style.height = width+"px";
    ele.setAttribute("class", "rotate90");
